7cb2ad2820de76c226d4a96e681161d5438cb685
PawelRa <pawelrat@poczta.onet.pl>
2022-09-02 11:00:46 +0200

    stringToArray

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println(Arrays.toString(CountByX.counter(1, 10)));
-        System.out.println(ReduceButGrow.multiplying(new int[]{4, 1, 1, 1, 4}));
+//        System.out.println(ReduceButGrow.multiplying(new int[]{4, 1, 1, 1, 4}));
+        System.out.println(Arrays.toString(StringToArray.stringToArray("Robin Singh")));
     }
 }
