218b94cbb4b6f0589cfd6a2ce07ad52ab0285390
PawelRa <pawelrat@poczta.onet.pl>
2022-09-12 11:31:01 +0200

    SumArray one line

--- src/SumArray.java
+++ src/SumArray.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
+
 public class SumArray {
     public static void main(String[] args) {
-        double[] a = new double[] { };
         System.out.println(SumArray.sum(new double[] {}));
         System.out.println(SumArray.sum(new double[] {-2.398}));
         System.out.println(SumArray.sum(new double[] {1.1, 2.2, 3.3}));
@@ -8,10 +9,11 @@ public class SumArray {
         System.out.println(SumArray.sum(new double[] {30, 89, 100, 101}));
     }
     public static double sum(double[] numbers) {
-        double result = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            result += numbers[i];
-        }
-        return result;
+//        double result = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            result += numbers[i];
+//        }
+//        return result;
+        return Arrays.stream(numbers).sum();
     }
 }
