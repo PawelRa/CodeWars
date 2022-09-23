8e23c7527a8efd780a52812c0e32653978658c29
PawelRa <pawelrat@poczta.onet.pl>
2022-09-23 18:21:50 +0200

    CalculateAverage

---
+++ src/CalculateAverage.java
@@ -0,0 +1,25 @@
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+
+
+//import java.util.Arrays;
+
+public class CalculateAverage {
+    public static void main(String[] args) {
+        System.out.println(CalculateAverage.find_average(new int[]{1, 2, 3, 4, 5, 6}));
+        System.out.println(CalculateAverage.find_average(new int[]{}));
+    }
+
+    public static double find_average(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        double result = 0.0;
+        for (int number : array) {
+            result += number;
+        }
+        return result / array.length;
+//        result = Arrays.stream(array).sum()/array.length;
+    }
+
+}
