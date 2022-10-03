7a7d88cc9a1198ae1b1fea31b9a00bf7fc7495cf
PawelRa <pawelrat@poczta.onet.pl>
2022-10-03 12:44:53 +0200

    BinaryAddition

---
+++ src/BinaryAddition.java
@@ -0,0 +1,11 @@
+public class BinaryAddition {
+    public static String binaryAddition(int a, int b) {
+        return Integer.toBinaryString(a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BinaryAddition.binaryAddition(1, 1));
+    }
+
+
+}
