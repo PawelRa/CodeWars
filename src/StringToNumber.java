b788d40ac2273efbc4417cf4c4e2b460f3ea2f2c
PawelRa <pawelrat@poczta.onet.pl>
2022-10-07 12:54:20 +0200

    StringToNumber

---
+++ src/StringToNumber.java
@@ -0,0 +1,9 @@
+public class StringToNumber {
+    public static void main(String[] args) {
+        System.out.println(stringToNumber("12345"));
+    }
+
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+}
