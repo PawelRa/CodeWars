b76c28ae8f2c483e74969bb8490549082f3ef617
PawelRa <pawelrat@poczta.onet.pl>
2022-10-16 12:28:40 +0200

    Sentence Smash refactor

--- src/kyu8/SentenceSmash.java
+++ src/kyu8/SentenceSmash.java
@@ -6,15 +6,15 @@ public class SentenceSmash {
         System.out.println(smash(new String[] { "Bilal", "Djaghout" }));
     }
     public static String smash(String... words) {
-        String result = "";
-        for (int i=0; i < words.length; i++) {
-            if (i != words.length-1) {
-            result += words[i];
-            result += " ";
-            } else {
-            result += words[i];
-            }
-        }
-        return result;
+//        String result = "";
+//        for (int i=0; i < words.length; i++) {
+//            if (i != words.length-1) {
+//            result += words[i];
+//            result += " ";
+//            } else {
+//            result += words[i];
+//            }
+//        }
+        return String.join(" ", words);
     }
 }
