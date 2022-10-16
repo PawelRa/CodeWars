1ad06616234682c971e0353da87634127f45f71a
PawelRa <pawelrat@poczta.onet.pl>
2022-10-16 12:23:32 +0200

    Sentence Smash

---
+++ src/kyu8/SentenceSmash.java
@@ -0,0 +1,20 @@
+package kyu8;
+
+
+public class SentenceSmash {
+    public static void main(String[] args) {
+        System.out.println(smash(new String[] { "Bilal", "Djaghout" }));
+    }
+    public static String smash(String... words) {
+        String result = "";
+        for (int i=0; i < words.length; i++) {
+            if (i != words.length-1) {
+            result += words[i];
+            result += " ";
+            } else {
+            result += words[i];
+            }
+        }
+        return result;
+    }
+}
