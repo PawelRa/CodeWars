d6d167bab241428986e1b449472fc16993d162f1
PawelRa <pawelrat@poczta.onet.pl>
2022-10-21 10:22:33 +0200

    OppositesAttract refactor

--- src/kyu8/OppositesAttract.java
+++ src/kyu8/OppositesAttract.java
@@ -9,6 +9,6 @@ public class OppositesAttract {
     }
 
     public static boolean isLove(final int flower1, final int flower2) {
-        return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0);
+        return (flower1 % 2 != flower2 % 2);
     }
 }
