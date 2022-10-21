9e9c23ca8089266d737eb1cd96f79c4f3bd9b3a7
PawelRa <pawelrat@poczta.onet.pl>
2022-10-21 10:21:23 +0200

    OppositesAttract

---
+++ src/kyu8/OppositesAttract.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+public class OppositesAttract {
+    public static void main(String[] args) {
+        System.out.println(OppositesAttract.isLove(1, 4));
+        System.out.println(OppositesAttract.isLove(2, 2));
+        System.out.println(OppositesAttract.isLove(1, 1));
+        System.out.println(OppositesAttract.isLove(0, 1));
+    }
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return (flower1 % 2 == 0 && flower2 % 2 != 0) || (flower1 % 2 != 0 && flower2 % 2 == 0);
+    }
+}
