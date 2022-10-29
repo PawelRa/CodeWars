748bad9dd57aa3a0509da1c8dd79a0ac1fb74b8b
PawelRa <pawelrat@poczta.onet.pl>
2022-10-29 12:09:53 +0200

    StringRepeat refactor

--- src/kyu8/StringRepeat.java
+++ src/kyu8/StringRepeat.java
@@ -8,14 +8,6 @@ public class StringRepeat {
     }
 
     public static String repeatStr(final int repeat, final String string) {
-        if (repeat == 0) {
-            return "";
-        } else {
-            String result = string;
-            for (int i = 1; i < repeat; i++) {
-                result += string;
-            }
-        return result;
-        }
+        return string.repeat(repeat);
     }
 }
