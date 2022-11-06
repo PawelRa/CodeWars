d6c89b6c601a49787342c10c73483c79686f1444
PawelRa <pawelrat@poczta.onet.pl>
2022-11-06 16:35:51 +0100

    IsItEven

---
+++ src/kyu8/IsItEven.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class IsItEven {
+    public static void main(String[] args) {
+        System.out.println(isEven(1));
+    }
+
+    public static boolean isEven(double n) {
+        return n % 2 == 0;
+    }
+}
