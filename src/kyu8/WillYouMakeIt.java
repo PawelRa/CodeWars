3ab2232a0cfbdbec1b771e6b8584a81a23b917eb
PawelRa <pawelrat@poczta.onet.pl>
2022-11-11 19:29:14 +0100

    WillYouMakeIt

---
+++ src/kyu8/WillYouMakeIt.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class WillYouMakeIt {
+    public static void main(String[] args) {
+        System.out.println(zeroFuel(50, 25, 2));
+        System.out.println(zeroFuel(100, 50, 1));
+    }
+    public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        return mpg*fuelLeft>=distanceToPump;
+    }
+}
