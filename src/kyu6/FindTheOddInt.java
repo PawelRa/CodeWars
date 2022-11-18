3a9beebbc781cb2438222bf63a16c1012ab69f8b
PawelRa <pawelrat@poczta.onet.pl>
2022-11-18 14:30:24 +0100

    FindTheOddInt one line result

--- src/kyu6/FindTheOddInt.java
+++ src/kyu6/FindTheOddInt.java
@@ -1,5 +1,7 @@
 package kyu6;
 
+import static java.util.Arrays.stream;
+
 public class FindTheOddInt {
     public static void main(String[] args) {
         System.out.println(findIt(new int[]{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5})); //5
@@ -11,23 +13,23 @@ public class FindTheOddInt {
     }
 
     public static int findIt(int[] a) {
-        if (a.length == 1) {
-            return a[0];
-        }
-        int temp = 0;
-        int result = a[0];
-        for (int j : a) {
-            result = j;
-            for (int k : a) {
-                if (k == result) {
-                    temp++;
-                }
-            }
-            if (temp % 2 != 0) {
-                return result;
-            }
-            temp = 0;
-        }
-        return -1;
+//        if (a.length == 1) {
+//            return a[0];
+//        }
+//        int temp = 0;
+//        int result = a[0];
+//        for (int j : a) {
+//            result = j;
+//            for (int k : a) {
+//                if (k == result) {
+//                    temp++;
+//                }
+//            }
+//            if (temp % 2 != 0) {
+//                return result;
+//            }
+//            temp = 0;
+//        }
+        return stream(a).reduce(0, (x, y) -> x ^ y);
     }
 }
