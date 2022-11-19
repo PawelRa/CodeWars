006b20b9ff7d3505be6bb59a3afc546aaad9b36a
PawelRa <pawelrat@poczta.onet.pl>
2022-11-19 15:27:51 +0100

    FindMaximumAndMinimumValuesOfAList one line result

--- src/kyu8/FindMaximumAndMinimumValuesOfAList.java
+++ src/kyu8/FindMaximumAndMinimumValuesOfAList.java
@@ -1,5 +1,7 @@
 package kyu8;
 
+import java.util.Arrays;
+
 public class FindMaximumAndMinimumValuesOfAList {
     public static void main(String[] args) {
         System.out.println(min(new int[]{-52, 56, 30, 29, -54, 0, -110}));
@@ -9,29 +11,29 @@ public class FindMaximumAndMinimumValuesOfAList {
     }
 
     public static int min(int[] list) {
-        int min = list[0];
-        if (list.length == 1) {
-            return min;
-        }
-        for (int i = 1; i < list.length; i++) {
-            if (list[i] < min) {
-                min = list[i];
-            }
-        }
-        return min;
+//        int min = list[0];
+//        if (list.length == 1) {
+//            return min;
+//        }
+//        for (int i = 1; i < list.length; i++) {
+//            if (list[i] < min) {
+//                min = list[i];
+//            }
+//        }
+        return Arrays.stream(list).min().getAsInt();
     }
 
     public static int max(int[] list) {
-        int max = list[0];
-        if (list.length == 1) {
-            return max;
-        }
-        for (int i = 1; i < list.length; i++) {
-            if (list[i] > max) {
-                max = list[i];
-            }
-        }
-        return max;
+//        int max = list[0];
+//        if (list.length == 1) {
+//            return max;
+//        }
+//        for (int i = 1; i < list.length; i++) {
+//            if (list[i] > max) {
+//                max = list[i];
+//            }
+//        }
+        return Arrays.stream(list).max().getAsInt();
     }
 
 }
