6a963ea7ffb719ce651ce228eeafb2689758a1e1
PawelRa <pawelrat@poczta.onet.pl>
2022-12-11 22:54:05 +0100

    Consecutive strings step 2

--- src/kyu6/ConsecutiveStrings.java
+++ src/kyu6/ConsecutiveStrings.java
@@ -1,22 +1,26 @@
+//https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/java
+
 package kyu6;
+
+import java.util.Arrays;
 
 public class ConsecutiveStrings {
     public static void main(String[] args) {
-//        System.out.println(longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2));
-        System.out.println(longestConsec(new String[]{"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1));
-//        System.out.println(longestConsec(new String[] {}, 3));
-//        System.out.println(longestConsec(new String[] {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2));
-//        System.out.println(longestConsec(new String[] {"wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"}, 2));
-//        System.out.println(longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2));
-//        System.out.println(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 3));
-//        System.out.println(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15));
-//        System.out.println(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 0));
+//        System.out.println(longestConsec(new String[]{"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2));
+//        System.out.println(longestConsec(new String[]{"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1));
+//        System.out.println(longestConsec(new String[]{}, 3));
+//        System.out.println(longestConsec(new String[]{"itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2)); //wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck
+//        System.out.println(longestConsec(new String[]{"wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"}, 2));
+//        System.out.println(longestConsec(new String[]{"zone", "abigail", "theta", "form", "libe", "zas"}, -2));
+        System.out.println(longestConsec(new String[]{"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 3)); //ixoyx3452zzzzzzzzzzzz
+//        System.out.println(longestConsec(new String[]{"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 15));
+//        System.out.println(longestConsec(new String[]{"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"}, 0));
 
     }
 
     public static String longestConsec(String[] strarr, int k) {
-        if (strarr.length == 0 || k < 1) {
-            return "P";
+        if (strarr.length == 0 || k < 1 || k > strarr.length) {
+            return "";
         }
         String result = "";
         if (k == 1) {
@@ -28,13 +32,22 @@ public class ConsecutiveStrings {
             }
             return result;
         }
-        String[] temp = new String[k - 1];
-        for (int i = 0; i < strarr.length; i++) {
-            for (int j=0;j<k;j++){
+        String[] temp = new String[strarr.length - 1];
+        for (int i = 0; i < strarr.length - 1; i++) {
+            temp[i] = "";
+            for (int j = 0 + i; j < k + i; j++) {
                 temp[i] += strarr[j];
             }
         }
-
+        for (String s : temp) {
+            if (s == null) {
+                continue;
+            }
+            if (s.length() > result.length()) {
+                result = s;
+            }
+        }
+        System.out.println(Arrays.toString(temp));
         return result;
     }
 }
