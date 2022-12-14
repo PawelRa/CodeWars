8e4b8f53f17b27b1ebe7667d05bc7e6331e5148a
PawelRa <pawelrat@poczta.onet.pl>
2022-12-14 14:39:54 +0100

    Primes in numbers

--- src/kyu5/PrimesInNumbers.java
+++ src/kyu5/PrimesInNumbers.java
@@ -1,5 +1,4 @@
 package kyu5;
-//https://www.codewars.com/kata/54d512e62a5e54c96200019e/train/java
 //Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
 //        "(p1**n1)(p2**n2)...(pk**nk)"
 //        with the p(i) in increasing order and n(i) empty if n(i) is 1.
@@ -12,7 +11,8 @@ import java.util.Collections;
 public class PrimesInNumbers {
     public static void main(String[] args) {
         System.out.println(factors(86240));
-//        System.out.println(factors(3));
+        System.out.println(factors(7775460)); //"(2**2)(3**3)(5)(7)(11**2)(17)"
+        System.out.println(factors(3));
     }
 
     public static String factors(int n) {
@@ -27,12 +27,29 @@ public class PrimesInNumbers {
                     if (n % i == 0) {
                         tempList.add(i);
                         n = n / i;
+                        i = 2;
                     }
                 }
             }
         }
         Collections.sort(tempList);
-        System.out.println(tempList);
+        int tempPower = 1;
+        int tempNumber = tempList.get(0);
+        for (int i = 1; i <= tempList.size(); i++) {
+            if (i < tempList.size() && tempList.get(i) == tempNumber) {
+                tempPower++;
+            } else {
+                if (tempPower == 1) {
+                    result += "(" + tempNumber + ")";
+                } else {
+                    result += "(" + tempNumber + "**" + tempPower + ")";
+                }
+                if (i < tempList.size()) {
+                    tempNumber = tempList.get(i);
+                }
+                tempPower = 1;
+            }
+        }
         return result;
     }
 }
