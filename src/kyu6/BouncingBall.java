15294575158ebd007752566812ba812a0ea39305
PawelRa <pawelrat@poczta.onet.pl>
2022-12-17 09:19:38 +0100

    BouncingBall

--- src/kyu6/BouncingBall.java
+++ src/kyu6/BouncingBall.java
@@ -2,8 +2,9 @@ package kyu6;
 
 public class BouncingBall {
     public static void main(String[] args) {
-//        System.out.println(bouncingBall(3.0, 0.66, 1.5)); //3
+        System.out.println(bouncingBall(3.0, 0.66, 1.5)); //3
         System.out.println(bouncingBall(30.0, 0.66, 1.5)); //15
+        System.out.println(bouncingBall(2.0, 0.5, 1.0)); //1
     }
 
     public static int bouncingBall(double h, double bounce, double window) {
@@ -12,16 +13,13 @@ public class BouncingBall {
         }
         double tempH = h;
         int bounces = 0;
-        while (tempH > window){
+        while (tempH > window) {
             bounces++;
-            if (tempH * bounce >= window) {
+            if (tempH * bounce > window) {
                 bounces++;
-                tempH = tempH * bounce;
             }
-            System.out.println(bounces);
+            tempH = tempH * bounce;
         }
-
-
         return bounces;
     }
 }
