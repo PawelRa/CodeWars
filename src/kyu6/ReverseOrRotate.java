aa91458b481f8d2653143a7bff301df25406b493
PawelRa <pawelrat@poczta.onet.pl>
2022-12-25 17:08:19 +0100

    ReverseOrRotate result

--- src/kyu6/ReverseOrRotate.java
+++ src/kyu6/ReverseOrRotate.java
@@ -1,13 +1,14 @@
 package kyu6;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReverseOrRotate {
     public static void main(String[] args) {
-//        System.out.println(revRot("1234", 0)); //""
-//        System.out.println(revRot("", 0)); //""
-//        System.out.println(revRot("1234", 5)); //""
-//        System.out.println(revRot("733049910872815764", 5)); //"330479108928157"
+        System.out.println(revRot("1234", 0)); //""
+        System.out.println(revRot("", 0)); //""
+        System.out.println(revRot("1234", 5)); //""
+        System.out.println(revRot("733049910872815764", 5)); //"330479108928157"
         System.out.println(revRot("66443875", 4)); //"4466 8753"
     }
 
@@ -15,27 +16,30 @@ public class ReverseOrRotate {
         if (sz <= 0 || strng.length() < sz) {
             return "";
         }
-        String[] strngArray = strng.split("");
+        List<String> strngParts = new ArrayList<>();
         String result = "";
-        String temp = "";
-        for (int i = 0; i < strng.length(); i++) {
-            temp += strngArray[i];
-            if (temp.length() == sz) {
-                int testNumber = 0;
-                String[] tempArray = temp.split("");
-                for (int j = 0; j < tempArray.length; j++) {
-                    int x = Integer.parseInt(tempArray[j]);
-                    testNumber += x * x *x;
+        for (int i = 0; i < strng.length(); i += sz) {
+            strngParts.add(strng.substring(i, Math.min(strng.length(), i + sz)));
+        }
+        for (String strngPart : strngParts) {
+            if (strngPart.length() < sz) {
+                return result;
+            }
+            String[] temp = strngPart.split("");
+            int value = 0;
+            for (String s : temp) {
+                int x = Integer.parseInt(s);
+                value += x * x * x;
+            }
+            if (value % 2 == 0) {
+                for (int k = temp.length - 1; k >= 0; k--) {
+                    result += temp[k];
                 }
-                if (testNumber % 2 == 0) {
-                    for (int reverseArray = tempArray.length-1;reverseArray>=0;reverseArray--){
-                        result += tempArray[reverseArray];
-                    }
-                } else {
-                        System.out.println("przesuniecie");
+            } else {
+                for (int m = 1; m < temp.length; m++) {
+                    result += temp[m];
                 }
-                temp = "";
-                result += " ";
+                result += temp[0];
             }
         }
         return result;
