0889e54100bded07e4555526fea10e0a6035cc52
PawelRa <pawelrat@poczta.onet.pl>
2022-12-25 11:35:54 +0100

    ReverseOrRotate without reverse

--- src/kyu6/ReverseOrRotate.java
+++ src/kyu6/ReverseOrRotate.java
@@ -7,7 +7,8 @@ public class ReverseOrRotate {
 //        System.out.println(revRot("1234", 0)); //""
 //        System.out.println(revRot("", 0)); //""
 //        System.out.println(revRot("1234", 5)); //""
-        System.out.println(revRot("733049910872815764", 5)); //"330479108928157"
+//        System.out.println(revRot("733049910872815764", 5)); //"330479108928157"
+        System.out.println(revRot("66443875", 4)); //"4466 8753"
     }
 
     public static String revRot(String strng, int sz) {
@@ -15,7 +16,6 @@ public class ReverseOrRotate {
             return "";
         }
         String[] strngArray = strng.split("");
-        System.out.println(Arrays.toString(strngArray));
         String result = "";
         String temp = "";
         for (int i = 0; i < strng.length(); i++) {
@@ -25,16 +25,18 @@ public class ReverseOrRotate {
                 String[] tempArray = temp.split("");
                 for (int j = 0; j < tempArray.length; j++) {
                     int x = Integer.parseInt(tempArray[j]);
-                    testNumber += x * x;
+                    testNumber += x * x *x;
                 }
                 if (testNumber % 2 == 0) {
-                    System.out.println("reverse");
+                    for (int reverseArray = tempArray.length-1;reverseArray>=0;reverseArray--){
+                        result += tempArray[reverseArray];
+                    }
+                } else {
+                        System.out.println("przesuniecie");
                 }
-
-                result += temp;
                 temp = "";
+                result += " ";
             }
-            System.out.println("------------");
         }
         return result;
     }
