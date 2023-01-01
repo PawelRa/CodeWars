45ed852b46d11916984497ea8d3e28161264cffa
PawelRa <pawelrat@poczta.onet.pl>
2023-01-01 19:05:26 +0100

    Build a pile of Cubes result

--- src/kyu6/BuildAPileOfCubes.java
+++ src/kyu6/BuildAPileOfCubes.java
@@ -4,7 +4,7 @@ public class BuildAPileOfCubes {
     public static void main(String[] args) {
         System.out.println(findNb(1071225)); //45
         System.out.println(findNb(4183059834009L)); //2022
-//        System.out.println(findNb(24723578342962L)); //-1
+        System.out.println(findNb(24723578342962L)); //-1
         System.out.println(findNb(135440716410000L)); //4824
         System.out.println(findNb(40539911473216L));  //3568
         System.out.println(findNb(2304422822859502500L));  //55100
@@ -13,10 +13,13 @@ public class BuildAPileOfCubes {
     public static long findNb(long m) {
         long volume = 0;
         long result = 0;
-        for (long i = 0; volume <= m; i++) {
-            volume += Math.pow(i, 3);
+        for (long i = 1; volume < m; i++) {
+            volume += i * i * i;
             result = i;
         }
-        return result-1;
+        if (volume - m != 0) {
+            return -1;
+        }
+        return result;
     }
 }
