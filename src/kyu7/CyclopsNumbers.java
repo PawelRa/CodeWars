0cb972005249775ca4868510836566c7fc44fb4c
PawelRa <pawelrat@poczta.onet.pl>
2023-01-05 20:58:42 +0100

    Cyclops numbers refactor

--- src/kyu7/CyclopsNumbers.java
+++ src/kyu7/CyclopsNumbers.java
@@ -8,21 +8,6 @@ public class CyclopsNumbers {
         System.out.println(cyclops(2015));
     }
     public static boolean cyclops(long n) {
-        String binaryNumber = Long.toBinaryString(n);
-        System.out.println(binaryNumber);
-        if (binaryNumber.length()%2 ==0 || binaryNumber.length()<3){
-            return false;
-        }
-        String[] binaryNumberArray = binaryNumber.split("");
-        if (binaryNumberArray[(binaryNumberArray.length) / 2].equals("1")) {
-            return false;
-        }
-        System.out.println(binaryNumberArray.length/2);
-        for (int i =0; i< binaryNumberArray.length;i++){
-            if ((i != (binaryNumberArray.length)/2) && binaryNumberArray[i].equals("0")) {
-                return false;
-            }
-        }
-        return true;
+        return Long.toBinaryString(n).matches("^(1+)0\\1$");
     }
 }
