5b68e8cb8036644744616f843d357197c53a9446
PawelRa <pawelrat@poczta.onet.pl>
2023-01-08 15:06:09 +0100

    String incrementer result

--- src/kyu5/StringIncrementer.java
+++ src/kyu5/StringIncrementer.java
@@ -2,13 +2,13 @@ package kyu5;
 
 public class StringIncrementer {
     public static void main(String[] args) {
-//        System.out.println(incrementString("foobar000"));
-//        System.out.println(incrementString("foo19"));
-//        System.out.println(incrementString(""));
-//        System.out.println(incrementString("foobar99"));
-//        System.out.println(incrementString("VhyzMK0kR"));
+        System.out.println(incrementString("foobar000"));
+        System.out.println(incrementString("foo19"));
+        System.out.println(incrementString(""));
+        System.out.println(incrementString("foobar99"));
+        System.out.println(incrementString("VhyzMK0kR"));
         System.out.println(incrementString("fo99obar99"));
-
+        System.out.println(incrementString("?^@-"));
     }
 
     public static String incrementString(String str) {
@@ -16,10 +16,9 @@ public class StringIncrementer {
             return "1";
         }
         String[] tempString = str.split("");
-        if (Character.isLetter(tempString[str.length() - 1].charAt(0))) {
+        if (!(Character.isDigit(tempString[str.length() - 1].charAt(0)))) {
             return str + "1";
         }
-
         int add = 0;
         for (int i = tempString.length - 1; i >= 0; i--) {
             if (Character.isDigit(tempString[i].charAt(0))) {
@@ -41,9 +40,9 @@ public class StringIncrementer {
                 add = 0;
                 tempString[i] = tempString[i] + 1;
             }
-
-
-
+            if (!(Character.isDigit(tempString[i].charAt(0))) && add == 0) {
+                break;
+            }
         }
         String result = "";
         for (int i = 0; i < tempString.length; i++) {
