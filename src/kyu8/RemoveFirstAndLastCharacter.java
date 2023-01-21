40e72a2f2926fbb862fc32576ec8eaa7adee23da
PawelRa <pawelrat@poczta.onet.pl>
2023-01-21 22:19:27 +0100

    Remove First and Last Character - refactor

--- src/kyu8/RemoveFirstAndLastCharacter.java
+++ src/kyu8/RemoveFirstAndLastCharacter.java
@@ -6,12 +6,7 @@ public class RemoveFirstAndLastCharacter {
     }
 
     public static String remove(String str) {
-        String[] tempArray = str.split("");
-        String result = "";
-        for (int i = 1; i < tempArray.length - 1; i++) {
-            result += tempArray[i];
-        }
-        return result;
+        return str.substring(1, str.length() - 1);
     }
 }
 
