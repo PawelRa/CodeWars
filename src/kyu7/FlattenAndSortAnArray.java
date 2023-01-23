8a32b79cbfb5c223df4223659dabc1949692f4ae
PawelRa <pawelrat@poczta.onet.pl>
2023-01-23 19:59:11 +0100

    Flatten and sort an array - ArrayList

--- src/kyu7/FlattenAndSortAnArray.java
+++ src/kyu7/FlattenAndSortAnArray.java
@@ -1,30 +1,27 @@
 package kyu7;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class FlattenAndSortAnArray {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(flattenAndSort(new int[][]{})));
         System.out.println(Arrays.toString(flattenAndSort(new int[][]{{}, {1}})));
         System.out.println(Arrays.toString(flattenAndSort(new int[][]{{3, 2, 1}, {7, 9, 8}, {6, 4, 5}})));
+        System.out.println(Arrays.toString(flattenAndSort(new int[][]{{1, 3, 5} ,{100}, {2, 4, 6}})));
+        System.out.println(Arrays.toString(flattenAndSort(new int[][]{{111, 999}, {222}, {333}, {444}, {888}, {777}, {666}, {555}})));
     }
 
     public static int[] flattenAndSort(int[][] array) {
         if (array.length == 0) {
             return new int[]{};
         }
-        String tempString = "";
+        List<Integer> arrayTemp = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                tempString += array[i][j];
+                arrayTemp.add (array[i][j]);
             }
         }
-        String[] arrayTemp = tempString.split("");
-        int[] result = new int[arrayTemp.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = Integer.parseInt(arrayTemp[i]);
-        }
-        Arrays.sort(result);
-        return result;
+        Collections.sort(arrayTemp);
+        return  arrayTemp.stream().mapToInt(Integer::intValue).toArray();
     }
 }
