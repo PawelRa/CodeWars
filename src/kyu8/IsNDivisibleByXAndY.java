3a9196d54f4b5bceef016b2fa81dc3748f7116a1
PawelRa <pawelrat@poczta.onet.pl>
2023-02-18 19:48:54 +0100

    Is n divisible by x and y?

---
+++ src/kyu8/IsNDivisibleByXAndY.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class IsNDivisibleByXAndY {
+    public static void main(String[] args) {
+        System.out.println(isDivisible(12,4,3));
+        System.out.println(isDivisible(3,3,4));
+    }
+    public static boolean isDivisible(long n, long x, long y) {
+        return ((n%x==0)&&(n%y==0));
+    }
+}
