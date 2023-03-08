0f95833ef066407467eb0474c4a74d4da8adb135
PawelRa <pawelrat@poczta.onet.pl>
2023-03-08 19:08:56 +0100

    Be Concise I - The Ternary Operator - shorter

--- src/kyu8/BeConciseITheTernaryOperator.java
+++ src/kyu8/BeConciseITheTernaryOperator.java
@@ -6,14 +6,7 @@ public class BeConciseITheTernaryOperator {
     }
 
     public static String describeAge(int age) {
-        if (age <= 12) {
-            return "You're a(n) kid";
-        } else if (age >= 13 && age <= 17) {
-            return "You're a(n) teenager";
-        } else if (age >= 18 && age <= 64) {
-            return "You're a(n) adult";
-        } else {
-            return "You're a(n) elderly";
-        }
+        String name = "You're a(n) ";
+        return String.format("%s" + (age <= 12 ? "kid" : age <= 17 ? "teenager" : age <= 64 ? "adult" : "elderly"), name);
     }
 }
